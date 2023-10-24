427da69e719ab0bc751fff74d463f4b0881fc1ea
Lance Lopez <lancelopez46@gmail.com>
2023-10-24 17:54:19 +0800

    add message form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date(),
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date(),
+	},
+];
+
+router.get("/", (req, res, next) => {
+	res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+	res.render("form", { title: "New Message" });
+});
+
+router.post("/new", (req, res, next) => {
+	messages.push({
+		text: req.body.message,
+		user: req.body.username,
+		added: new Date(),
+	});
+	res.redirect("/");
 });
 
 module.exports = router;
